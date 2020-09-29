0a31021946ebdefdbcfe08d4b83d28bb705a3562
citrinaa <matasdziugas@gmail.com>
2020-09-29 17:29:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.tvMain = findViewById(R.id.tvMainLabel);
+    }
+
+    public void onBtnChangeClick(View view) {
+this.tvMain.setText("It works!");
+    }
+}
